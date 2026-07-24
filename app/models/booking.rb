59f8f61e541198b99ce3960956b2863ef268c090
v_tapakornn <outsource2615@setth.onmicrosoft.com>
2026-07-24 13:04:11 +0700

    feat: implement asynchronous booking processing using Sidekiq and Redis

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :event
 
+  enum :status, { pending: "pending", success: "success", failed: "failed" }, default: "pending"
+
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :quantity, presence: true, numericality: { greater_than: 0, only_integer: true }
 end
